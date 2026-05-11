42e40bd71be45428d82aa1fddd8734b446c10129
ideamule-gif <ideamule@gmail.com>
2026-05-11 07:54:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE = 'sprint-v1';
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/admin.html','/manifest.json'])));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
